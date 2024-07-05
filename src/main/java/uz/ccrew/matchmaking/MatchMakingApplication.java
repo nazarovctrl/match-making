5904c4cd87747be45a3f034bc419d4966c662178
Azimjon Nazarov <109890132+nazarovv2@users.noreply.github.com>
2024-07-05 19:34:38 +0500

    Initialize

---
+++ src/main/java/uz/ccrew/matchmaking/MatchMakingApplication.java
@@ -0,0 +1,13 @@
+package uz.ccrew.matchmaking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchMakingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchMakingApplication.class, args);
+	}
+
+}
