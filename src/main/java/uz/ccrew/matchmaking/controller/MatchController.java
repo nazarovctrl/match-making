7eab801af57aaf13750a3c5936a60998f263e1da
Azimjon Nazarov <109890132+nazarovv2@users.noreply.github.com>
2024-07-09 18:37:34 +0500

    Tz

--- src/main/java/uz/ccrew/matchmaking/controller/MatchController.java
+++ src/main/java/uz/ccrew/matchmaking/controller/MatchController.java
@@ -4,4 +4,10 @@ public class MatchController {
     // hamma turadgi match larni boshlash uchun kerak
     // agar team type solo bo'lmsa u holda matchni faqat leader boshlay oladi
     // match ni tugaganini ushlab oladigan va reyting hisoblaydigan api kerak faqat server uchun ishlaydi
+
+    /* API list
+    1. find match (notify when match starts) for Player(team leader)
+    2. add match result and calculate rank points for Server
+    3. get match result
+     */
 }
--- src/main/java/uz/ccrew/matchmaking/controller/ServerController.java
+++ src/main/java/uz/ccrew/matchmaking/controller/ServerController.java
@@ -2,4 +2,12 @@ package uz.ccrew.matchmaking.controller;
 
 public class ServerController {
     //server qoshish ochirish imkoniyati faqat adminstrator uchun
+
+    /* API list
+    1. add for Administrator
+    2. edit for Administrator
+    3. delete for Administrator
+    4. view for Admin, Player
+    5. list for Admin, Player
+    */
 }
