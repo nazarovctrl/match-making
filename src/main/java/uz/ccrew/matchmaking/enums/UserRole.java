1073cbc1a980815c74577c3ddd43b05e9319b2e3
Azimjon Nazarov <109890132+nazarovv2@users.noreply.github.com>
2024-07-12 14:43:35 +0500

    packaged changed

--- src/main/java/uz/ccrew/matchmaking/enums/UserRole.java
+++ src/main/java/uz/ccrew/matchmaking/enums/UserRole.java
@@ -1,5 +1,11 @@
 package uz.ccrew.matchmaking.enums;
 
+import java.util.Arrays;
+
 public enum UserRole {
     ADMINISTRATOR, SERVER, PLAYER;
+
+    public static String[] all() {
+        return Arrays.stream(UserRole.values()).map(Enum::name).toArray(String[]::new);
+    }
 }
