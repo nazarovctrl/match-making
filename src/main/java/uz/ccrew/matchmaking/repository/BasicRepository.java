d39ad2f279e8cc27eea3bdf8f86bbceaa39dbef9
Azimjon Nazarov <109890132+nazarovv2@users.noreply.github.com>
2024-07-13 14:22:07 +0500

    Fix

--- src/main/java/uz/ccrew/matchmaking/repository/BasicRepository.java
+++ src/main/java/uz/ccrew/matchmaking/repository/BasicRepository.java
@@ -2,8 +2,10 @@ package uz.ccrew.matchmaking.repository;
 
 import uz.ccrew.matchmaking.exp.NotFoundException;
 
+import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@NoRepositoryBean
 public interface BasicRepository<T, D> extends JpaRepository<T, D> {
     default T loadById(D id) {
         return findById(id).orElseThrow(() -> new NotFoundException("Not found"));
