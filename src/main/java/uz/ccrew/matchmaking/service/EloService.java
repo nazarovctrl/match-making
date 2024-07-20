0fa2529ee325f4b02967584de13d3f7f441564b0
maksud9909 <rmaksud822@gmail.com>
2024-07-20 11:17:17 +0500

    Elo business logic

--- src/main/java/uz/ccrew/matchmaking/service/EloService.java
+++ src/main/java/uz/ccrew/matchmaking/service/EloService.java
@@ -5,6 +5,6 @@ import uz.ccrew.matchmaking.entity.Player;
 import java.util.List;
 
 public interface EloService {
-    public void updateRatings(Player winner, Player loser);
-    public void updateRatings(List<Player> winners,List<Player> losers);
+    void updateRatings(Player winner, Player loser);
+    void updateRatings(List<Player> winners,List<Player> losers);
 }
--- src/main/java/uz/ccrew/matchmaking/service/impl/EloServiceImpl.java
+++ src/main/java/uz/ccrew/matchmaking/service/impl/EloServiceImpl.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public abstract class EloServiceImpl implements EloService {
+public class EloServiceImpl implements EloService {
     private final PlayerRepository playerRepository;
 
     @Override
