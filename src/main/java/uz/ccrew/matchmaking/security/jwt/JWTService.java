506490d50f946c8b05c69b14738d6921d4caf15c
maksud9909 <rmaksud822@gmail.com>
2024-08-01 13:39:11 +0500

    SQL

--- src/main/java/uz/ccrew/matchmaking/security/jwt/JWTService.java
+++ src/main/java/uz/ccrew/matchmaking/security/jwt/JWTService.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Date;
